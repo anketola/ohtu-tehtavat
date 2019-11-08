715086f6523a2b55b90e315a891480bab9d8863a
Antti Ketola <antti@anttiketola.com>
2019-11-08 20:53:40 +0200

    Tehtävät 11 ja 12 tehty

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -5,17 +5,17 @@ import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        Viitegeneraattori viitegen = new Viitegeneraattori();
-        Kirjanpito kirjanpito      = new Kirjanpito();
-        Varasto varasto            = new Varasto(kirjanpito);
-        Pankki pankki              = new Pankki(kirjanpito);
-        Kauppa kauppa              = new Kauppa(varasto, pankki, viitegen);
-        
-        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
+
+       // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
@@ -32,7 +32,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
-        for (String tapahtuma : kirjanpito.getTapahtumat()) {
+        for (String tapahtuma :  ctx.getBean(Kirjanpito.class).getTapahtumat()) {
             System.out.println(tapahtuma);
         }
     }
