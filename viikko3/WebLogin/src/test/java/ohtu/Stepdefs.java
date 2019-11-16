f551270917504f29ca2a3dba5c15ba0d37323f9c
Antti Ketola <antti@anttiketola.com>
2019-11-16 19:00:56 +0200

    Tehtävä 10 tehty

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -18,10 +18,38 @@ public class Stepdefs {
     
     @Given("login is selected")
     public void loginIsSelected() {
-        driver.get(baseUrl);
+        enterFrontpage();
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();   
     }    
+    
+    @Given("command new user is selected")
+    public void commandNewUserIsSelected() {
+        enterFrontpage();
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();   
+    }
+    
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void aValidUsernameAndPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @When("an invalid username {string} and valid password {string} and matching password confirmation are entered")
+    public void anInvalidUsernameAndValidPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and a too short password {string} and matching password confirmation are entered")
+    public void aValidUsernameAndAnTooShortPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+
+    @When("a valid username {string} and a valid password {string} and non-matching confirmation are entered")
+    public void aValidUsernameAndAValidPasswordAndNonMatchingConfirmationAreEntered(String username, String password) {
+        registerWith(username, password, password + "abc");
+    }
+
     
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
@@ -43,6 +71,17 @@ public class Stepdefs {
         logInWith(username, "abc12345678");
     }     
     
+    @Then("a new user is created")
+    public void aNewUserIsCreated() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+
+    @Then("user is not created and error {string} is reported")
+    public void userIsNotCreatedAndErrorIsReported(String error) {
+        pageHasContent(error);
+    }
+
+    
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
@@ -61,10 +100,14 @@ public class Stepdefs {
         
     /* helper methods */
  
+    public void enterFrontpage() {
+        driver.get(baseUrl);
+    }
+    
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
-        
+    
     private void logInWith(String username, String password) {
         assertTrue(driver.getPageSource().contains("Give your credentials to login"));
         WebElement element = driver.findElement(By.name("username"));
@@ -72,6 +115,18 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
+        element.submit();  
+    }
+    
+    private void registerWith(String username, String password, String confirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(confirmation);
+        element = driver.findElement(By.name("signup"));
         element.submit();  
     } 
 }
